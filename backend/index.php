0590bb0ead1ba72ccccb9d8d45caff4543cbe5f7
Johan Strootman <johanstr@users.noreply.github.com>
2023-06-09 12:28:41 +0200

    Routing hersteld en index.php aangepast.
    index.php aangepast, aanpak lijkt al meer op de RequestHandler die we nog gaan ontwikkelen.

--- backend/index.php
+++ backend/index.php
@@ -3,20 +3,21 @@
 
 $routes = include('app/Routes/routes.php');
 
-$request_type = $_SERVER['REQUEST_METHOD'];
-$request_uri = $_SERVER['REQUEST_URI'];
+$request_type     = $_SERVER['REQUEST_METHOD'];
+$request_resource = isset($_GET['resource']) ? $_GET['resource'] : '/';
+$request_id       = isset($_GET['id']) ? intval($_GET['id']) : null;
 
-echo "Request type: $request_type<br />";
-echo "Request URI: $request_uri<br />";
-
-$controller = $routes[$request_type][$request_uri][0];
-$method = $routes[$request_type][$request_uri][1];
+$controller = $routes[$request_type][$request_resource][0];
+$method     = $routes[$request_type][$request_resource][1];
 
 echo '<pre>';
 if(class_exists($controller)) {
    $instance = new $controller();
    if(method_exists($instance, $method)) 
-      print_r($instance->$method(1));
+      if(! is_null($request_id))
+         print_r($instance->$method($request_id));
+      else
+         print_r($instance->$method());
    else
       echo "<span style='font-size: bold; color: red;'>ERROR:</span> Method <span style='color: red;'>$method</span> in class <span style='color: orange;'>$controller</span> doesn't exist<br />";
 } else 
